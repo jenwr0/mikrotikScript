e75a2ba69e5faef050331b8134374ac712a80dd7
jenwr0 <qw.bubo@gmail.com>
2025-09-27 16:25:40 +0700

    mikrotik-limiter.js

--- mikrotik-limiter.js
+++ mikrotik-limiter.js
@@ -1,24 +1,39 @@
-### -----MirotikLimiter
-### -- I use custom queue tree and firewall mangle
+# ===== MIKROTIK BW DYNAMIC LIMITER ===== #
+# need queue trees and firewall mangle
 
-:local limits { 1=62914560; 2=15728640; 3=10485760; 4=7864320; 5=6291456; 6=5242880 };
-:local subnet "192.168.3.0/24";
-:local prefix "U-";
-:local suffix { "-A"; "-B" };
-:local activeCount [:tostr [:len [/ip hotspot active find where address in $subnet]]];
-:log info "Hotspot aktif: $activeCount"; 
-:local todayLimit ($limits->$activeCount);
-:if ($todayLimit = "") do={
-    :set todayLimit 5242880; 
-};
-:foreach id in=[/ip hotspot active find where address in $subnet] do={
-    :local ip [/ip hotspot active get $id value-name=address];
-    :local qA ("$prefix$ip" . ($suffix->0));
-    :local qB ("$prefix$ip" . ($suffix->1));
-    :if ([/queue tree get [find name=$qA] max-limit] != $todayLimit) do={
-        /queue tree set [find name=$qA] max-limit=$todayLimit;
-        /queue tree set [find name=$qB] max-limit=$todayLimit;
-    }
+# 35Mbps Base ISP simetric bandwidth
+:local base 35000000 
+:local subnet "192.168.3.0/24" # I used 192.168.255.0 for guest user (Static bw)
+:local prefix "U-"
+:local suffix {"-A"; "-B"}
 
+:local activeList [/ip hotspot active find where address in $subnet]
+:local activeCount [:len $activeList]
+
+:local todayLimit ($base)
+:if ($activeCount >= 1 && $activeCount <= 6) do={
+    :set todayLimit ($base / $activeCount)
 }
 
+
+# Convert to Mbps
+:local mbps ($todayLimit / 1000000);:local mbpsStr [:pick ($mbps . "") 0 ([:find ($mbps . "") "."] + 3)];
+
+
+:log info ("Hotspot aktif: $activeCount | Limit per user: $mbpsStr Mbps")
+
+:foreach id in=$activeList do={
+    :local ip [/ip hotspot active get $id value-name=address]
+    :local qA ("$prefix$ip" . ($suffix->0))
+    :local qB ("$prefix$ip" . ($suffix->1))
+
+    :local currentLimitA [/queue tree get [find name=$qA] value-name=max-limit]
+    :local currentLimitB [/queue tree get [find name=$qB] value-name=max-limit]
+
+    # Update jika salah satu berbeda dari todayLimit
+    :if (($currentLimitA != $todayLimit) || ($currentLimitB != $todayLimit)) do={
+        :log info "Update limit untuk $ip: Upload=$currentLimitA, Download=$currentLimitB â†’ $todayLimit"
+        /queue tree set [find name=$qA] max-limit=$todayLimit
+        /queue tree set [find name=$qB] max-limit=$todayLimit
+    }
+}
